8fc7a8d031cdedda03a707937e42352b936904d5
Kanjamad Bosel <npor25@yahoo.com>
2019-02-21 21:22:40 -0800

    working on signin

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('this is working');
 })
 
+app.post('/signin', (req, res) => {
+    res.send('signing')
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
