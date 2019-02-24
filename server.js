9c038951323b374afe5b4b1e0f0270a2d477771f
Kanjamad Bosel <npor25@yahoo.com>
2019-02-23 17:43:57 -0800

    Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- server.js
+++ server.js
@@ -51,6 +51,17 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    database.users.forEach(user => {
+        if (user.id == id ) {
+            res.json(user);
+        }else {
+            res.status(404).json('no such user');
+        }
+    })
+})
+
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 })
